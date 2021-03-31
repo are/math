162cbececc38339d721d4369134172dbebe5efd6
are <me@iama.re>
2021-03-31 03:52:20 +0200

    feat: finish initial Beatty sequence

--- src/common/canvas.js
+++ src/common/canvas.js
@@ -1,7 +1,8 @@
-export function responsiveCanvasHook(canvas) {
+export function responsiveCanvasHook(canvas, fn) {
   function adjustSize() {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
+    fn()
   }
 
   window.addEventListener('resize', adjustSize)
